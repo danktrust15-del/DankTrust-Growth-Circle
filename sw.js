0390b906ba5fdc433909119b540855d0cf33d2ec
danktrust15-del <danktrust15@gmail.com>
2026-09-13 00:19:21 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME = "danktrust-v2";
+const FILES = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./logo.png"];
+self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES))); self.skipWaiting(); });
+self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(k => Promise.all(k.map(x => { if(x !== CACHE_NAME) return caches.delete(x); })))); self.clients.claim(); });
+self.addEventListener("fetch", e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
